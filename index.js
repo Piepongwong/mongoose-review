cc083c4ce56ca17a019d97108a4130ed43314fcd
Jurgen <j.tonneyck@gmail.com>
2020-12-02 21:01:39 +0100

    basic version done

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Run your code here, after you have insured that the connection was made and the database has been dropped
+    return Recipe.create({
+      title: "Flash Seared Skirt Steak",
+      level: "Amateur Chef",
+      ingredients: [
+        "10oz Skirt Steak",
+        "3 cloves of garlic",
+        "3 tablespoons olive oil",
+        "3 tablespoons soy sauce",
+        "3 tablespoons balsamic vinaigrette",
+        "5 tablespoons honey",
+      ],
+      cuisine: "Steakhouse",
+      dishType: "breakfast",
+      image: "https://images.media-allrecipes.com/images/75131.jpg",
+      duration: 240,
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((result) => {
+    return Recipe.insertMany(data).then((recipes) => {
+      console.log(recipes);
+    });
+  })
+  .then((result) => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      {new: true}
+    );
+  })
+  .then((recipe) => {
+    return Recipe.findByIdAndDelete({title: "Carrot Cake"})
+  })
+  .then((recipe)=> {
+    console.log("done");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
