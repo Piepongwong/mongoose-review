a0723c66cc882dd78ce716d91fd0eb2c363bd152
Jurgen <j.tonneyck@gmail.com>
2020-12-02 21:22:42 +0100

    done

--- index.js
+++ index.js
@@ -20,40 +20,49 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
+    let promises = [];
     // Run your code here, after you have insured that the connection was made and the database has been dropped
-    return Recipe.create({
-      title: "Flash Seared Skirt Steak",
-      level: "Amateur Chef",
-      ingredients: [
-        "10oz Skirt Steak",
-        "3 cloves of garlic",
-        "3 tablespoons olive oil",
-        "3 tablespoons soy sauce",
-        "3 tablespoons balsamic vinaigrette",
-        "5 tablespoons honey",
-      ],
-      cuisine: "Steakhouse",
-      dishType: "breakfast",
-      image: "https://images.media-allrecipes.com/images/75131.jpg",
-      duration: 240,
-    });
-  })
-  .then((result) => {
-    return Recipe.insertMany(data).then((recipes) => {
-      console.log(recipes);
-    });
-  })
-  .then((result) => {
-    return Recipe.findOneAndUpdate(
-      { title: "Rigatoni alla Genovese" },
-      { duration: 100 },
-      {new: true}
+    promises.push(
+      Recipe.create({
+        title: "Flash Seared Skirt Steak",
+        level: "Amateur Chef",
+        ingredients: [
+          "10oz Skirt Steak",
+          "3 cloves of garlic",
+          "3 tablespoons olive oil",
+          "3 tablespoons soy sauce",
+          "3 tablespoons balsamic vinaigrette",
+          "5 tablespoons honey",
+        ],
+        cuisine: "Steakhouse",
+        dishType: "breakfast",
+        image: "https://images.media-allrecipes.com/images/75131.jpg",
+        duration: 240,
+      })
     );
+
+    promises.push(Recipe.insertMany(data));
+
+    return Promise.all(promises);
+  })
+  .then((recipes) => {
+    let promises = [];
+
+    promises.push(
+      Recipe.findOneAndUpdate(
+        { title: "Rigatoni alla Genovese" },
+        { duration: 100 },
+        { new: true }
+      )
+    );
+
+    promises.push(
+      Recipe.findOneAndDelete({ title: "Flash Seared Skirt Steak" })
+    );
+
+    return Promise.all(promises);
   })
   .then((recipe) => {
-    return Recipe.findByIdAndDelete({title: "Carrot Cake"})
-  })
-  .then((recipe)=> {
     console.log("done");
   })
   .catch((error) => {
