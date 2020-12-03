52c0d2d7ac332f053a7d5e3a24a1a5172d81b96b
Jurgen <j.tonneyck@gmail.com>
2020-12-04 00:02:55 +0100

    great

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
-  title: String,
+  title: { type: String, required: true},
   level: {
     type: String,
     enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
