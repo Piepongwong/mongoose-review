f93c625b64fd47d351a11714994bba67851c7783
Jurgen <j.tonneyck@gmail.com>
2020-12-03 20:21:10 +0100

    simple form

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const cors = require("cors");
 const Recipe = require("./models/Recipe.model");
 const data = require("./data");
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+const bodyParser = require("body-parser");
 
+app.options('*', cors())
 app.use(cors());
+app.use(bodyParser.json());
 
 mongoose
   .connect(MONGODB_URI, {
@@ -21,25 +24,41 @@ mongoose
     console.log("err", err);
   });
 
-app.get("/recipes", (req, res) => {
 
+
+
+
+app.post("/recipes", (req, res) => {
+  Recipe.create(req.body)
+  .then((recipe)=> {
+    res.send("Recipe created!")
+  })
+  .catch((err)=> {
+    res.status(500).send("ooooeps")
+  })
+});
+
+
+
+
+app.get("/recipes", (req, res) => {
   Recipe.find({})
     .then((recipes) => {
       res.status(200).json(recipes);
     })
     .catch((err) => {
-      res.status(500).json({message: "ooooeps"})
+      res.status(500).json({ message: "ooooeps" });
     });
 });
 
 app.get("/recipes/:id", (req, res) => {
   Recipe.findById(req.params.id)
     .then((recipe) => {
-      if(!recipe) res.status(404).json({message: "No such recipe"})
+      if (!recipe) res.status(404).json({ message: "No such recipe" });
       else res.json(recipe);
     })
     .catch((err) => {
-      res.status(500).json({message: "ooooeps"})
+      res.status(500).json({ message: "ooooeps" });
     });
 });
 
